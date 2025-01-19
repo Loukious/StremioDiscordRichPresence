7a8e2eb3815d258f0f719178b1cfadf9d39af6b4
Loukious <Loukious@users.noreply.github.com>
2025-01-19 14:08:12 +0100

    feat: Implement configuration loading from INI file for Discord presence settings

--- dllmain.cpp
+++ dllmain.cpp
@@ -6,13 +6,14 @@
 #include "ios"
 #include "fstream"
 #include "Discord.h"
-
+#include "ini.h"
 #include "MemoryUtility.h"
 #include <mutex>
 #include <psapi.h>
 #include <format>
 #include "Video.h"
 #include <iostream>
+using namespace mINI;
 
 
 static Discord* g_discord;
@@ -24,16 +25,68 @@ static std::mutex initMutex;
 // Remove this line if you aren't proxying any functions.
 HMODULE hModule = LoadLibrary(L"C:\\Windows\\SysWOW64\\DWrite.dll");
 
+static bool show_buttons = true;
+static std::string link_target = "app";
+static bool disable_in_menu = false;
+static bool disable_when_paused = false;
+static bool enable_console = false;
+static int refresh_interval = 10;
+static time_t g_startTime;
 
-inline static bool FileExists(const std::string& name) {
-	struct stat buffer;
-	return (stat(name.c_str(), &buffer) == 0);
+
+static std::string toLowerCase(const std::string& str) {
+	std::string result = str;
+	std::transform(result.begin(), result.end(), result.begin(), ::tolower);
+	return result;
+}
+
+static void ReadConfig()
+{
+	INIFile config(".\\RPCconfig.ini");
+	INIStructure ini;
+
+
+	if (config.read(ini)) {
+		// Successfully read the INI file, load the values
+		show_buttons = ini["Buttons"]["show_buttons"] == "true";
+		link_target = toLowerCase(ini["Buttons"]["link_target"]);
+		if (link_target != "app" && link_target != "web") {
+			link_target = "app"; // Default to "app" if invalid
+		}
+		disable_in_menu = ini["Activity"]["disable_in_menu"] == "true";
+		disable_when_paused = ini["Activity"]["disable_when_paused"] == "true";
+		enable_console = ini["Debug"]["enable_console"] == "true";
+		refresh_interval = std::stoi(ini["Activity"]["refresh_interval"]);
+		if (refresh_interval < 1 || refresh_interval > 60) {
+			refresh_interval = 10; // Default to 10 seconds if invalid
+		}
+
+	}
+	else {
+		// Failed to read the INI file, initialize with default values and create the file
+		ini["Buttons"]["show_buttons"] = show_buttons ? "true" : "false";
+		ini["Buttons"]["link_target"] = link_target;
+		ini["Activity"]["disable_in_menu"] = disable_in_menu ? "true" : "false";
+		ini["Activity"]["disable_when_paused"] = disable_when_paused ? "true" : "false";
+		ini["Debug"]["enable_console"] = enable_console ? "true" : "false";
+		ini["Activity"]["refresh_interval"] = std::to_string(refresh_interval);
+
+		// Write default configuration to file
+		if (!config.write(ini, true)) {
+			std::cerr << "Error: Unable to write default configuration to RPCconfig.ini" << std::endl;
+		}
+	}
+
 }
 
 static void UpdateDiscordPresence() {
 	std::string oldinfo = "";
+	double previousVideoTime = -1;
+	bool isPaused = false;
 
 	while (true) {
+		std::this_thread::sleep_for(std::chrono::seconds(refresh_interval));
+
 		double videoTime = g_memoryUtility->ReadVideoTime();
 
 		if (videoTime > 0) {
@@ -47,8 +100,17 @@ static void UpdateDiscordPresence() {
 				vid = Video(); // Reinitialize video object with new video info
 			}
 
+			if (videoTime == previousVideoTime) {
+				isPaused = true;
+			}
+			else {
+				isPaused = false;
+				previousVideoTime = videoTime;
+			}
+
 			std::time_t currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 			int difference = static_cast<int>(currentTime) - static_cast<int>(videoTime);
+			std::time_t endTime = currentTime + static_cast<int>(vid.getmaxtime() - vid.gettime());
 
 			std::string state;
 			std::string details;
@@ -57,44 +119,68 @@ static void UpdateDiscordPresence() {
 			std::string smallImageKey;
 			std::string smallImageText;
 			std::string IMBdUrl = "https://www.imdb.com/title/" + vid.id;
-			std::string StremioUrl = "stremio:///detail/" + vid.type + "/" + vid.id + "/" + currentInfoStr;
+			std::string StremioUrl = "";
+			if (link_target == "web") {
+				StremioUrl = "https://web.stremio.com/#/detail/" + vid.type + "/" + vid.id + "/" + currentInfoStr;
+			}
+			else {
+				StremioUrl = "stremio:///detail/" + vid.type + "/" + vid.id + "/" + currentInfoStr;
+			}
+
 
 			if (vid.type == "series") {
-				state = vid.name + " (S" + vid.s + "-E" + vid.ep + ")";
-				details = "Watching Series";
-				largeImageKey = vid.poster;
+				details = vid.name;
+				state = isPaused ? "Paused" : vid.epname + " (S" + vid.s + "-E" + vid.ep + ")";
 				largeImageText = vid.name + " " + vid.year;
 				smallImageKey = vid.thumbnail;
 				smallImageText = vid.epname;
-				discordRichPresence.startTimestamp = difference;
 			}
 			else {
-				state = vid.name + " " + vid.year;
-				details = "Watching a Movie";
-				largeImageKey = vid.poster;
+				details = vid.name;
+				state = isPaused ? "Paused" : vid.year;
 				largeImageText = vid.name + " " + vid.year;
-				discordRichPresence.startTimestamp = difference;
 			}
 
+			largeImageKey = vid.poster;
+
+			if (!isPaused) {
+				discordRichPresence.startTimestamp = difference;
+				discordRichPresence.endTimestamp = endTime;
+			}
+			else if (disable_when_paused) {
+				g_discord->clearPresence();
+				continue;
+			}
+
+			discordRichPresence.type = DISCORD_ACTIVITY_TYPE_WATCHING;
 			discordRichPresence.state = state.c_str();
 			discordRichPresence.details = details.c_str();
 			discordRichPresence.largeImageKey = largeImageKey.c_str();
 			discordRichPresence.largeImageText = largeImageText.c_str();
 			discordRichPresence.smallImageKey = smallImageKey.c_str();
 			discordRichPresence.smallImageText = smallImageText.c_str();
-			discordRichPresence.button1Label = "More details";
-			discordRichPresence.button1Url = IMBdUrl.c_str();
-			discordRichPresence.button2Label = "Watch on Stremio";
-			discordRichPresence.button2Url = StremioUrl.c_str();
-
-
+			if (show_buttons) {
+				discordRichPresence.button1Label = "More details";
+				discordRichPresence.button1Url = IMBdUrl.c_str();
+				discordRichPresence.button2Label = "Watch on Stremio";
+				discordRichPresence.button2Url = StremioUrl.c_str();
+			}
+			g_discord->update(discordRichPresence);
+		}
+		else if (!disable_in_menu) {
+			DiscordRichPresence discordRichPresence;
+			memset(&discordRichPresence, 0, sizeof(discordRichPresence));
+			discordRichPresence.type = DISCORD_ACTIVITY_TYPE_WATCHING;
+			discordRichPresence.state = "In Menu";
+			discordRichPresence.details = "Browsing catalog";
+			discordRichPresence.largeImageKey = "https://avatars.githubusercontent.com/u/13152917";
+			discordRichPresence.largeImageText = "Stremio";
+			discordRichPresence.startTimestamp = g_startTime;
 			g_discord->update(discordRichPresence);
 		}
 		else {
-			g_discord->initialize(false);
+			g_discord->clearPresence();
 		}
-
-		std::this_thread::sleep_for(std::chrono::seconds(10));
 	}
 }
 
@@ -103,8 +189,8 @@ static void UpdateDiscordPresence() {
 
 
 DWORD WINAPI MainThread(LPVOID param) {
-
-    g_discord->initialize();
+	
+	g_discord->initialize();
 
 	g_memoryUtility->initialize();
 
@@ -171,8 +257,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	{
 	case DLL_PROCESS_ATTACH:
 		if (GetProcessName() == "stremio-shell-ng.exe") {
-			// Check if the specific file exists
-			if (FileExists("enable_console.txt")) {
+			ReadConfig();
+			g_startTime = std::time(0);
+			if (enable_console) {
 				AllocConsole(); // Allocate a console
 				freopen_s((FILE**)stdout, "CONOUT$", "w", stdout); // Redirect stdout to console
 				freopen_s((FILE**)stderr, "CONOUT$", "w", stderr); // Redirect stderr to console
